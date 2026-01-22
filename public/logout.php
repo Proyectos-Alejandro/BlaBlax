ff4ba1f3b6a85940d379f7d9d548d285e098d74a
alejandro <alejandro.cal.mat@gmail.com>
2026-01-22 19:10:37 +0100

    Cambio en login.php encriptado de contraseñas TUTO(https://www.youtube.com/watch?v=jUMcoJ1vDC0)

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-session_destroy(); // Borra la sesión
-header("Location: index.php"); // Te devuelve al index
+session_destroy(); 
+header("Location: index.php"); 
 exit();
 ?>
